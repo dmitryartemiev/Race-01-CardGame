18b347a2bd4ad6f00bc464080fba0cc659953a94
dmitryartemiev <dima77261@gmail.com>
2021-02-17 17:51:06 +0200

    random decks are ready

--- public/js/drag'n'drop.js
+++ public/js/drag'n'drop.js
@@ -1,52 +1,53 @@
 "use strict";
-const dragAndDrop = () => {
-  const cards = document.querySelectorAll(".card");
-  const empties = document.querySelectorAll(".empty");
 
+function dragAndDrop (){
+  const cards = document.getElementsByClassName("card");
+  const empties = document.querySelectorAll(".empty");
   let transfer;
 
   for (let card of cards) {
     const dragStart = function () {
-        transfer = this;
+      console.log(card);
+      transfer = this;
       setTimeout(() => {
         this.classList.add("hide");
       }, 1);
+      console.log(transfer);
     };
     const dragEnd = function () {
-        setTimeout(() => {
-          this.classList.remove("hide");
-        }, 0);
-      };
+      setTimeout(() => {
+        this.classList.remove("hide");
+      }, 0);
+    };
     card.addEventListener("dragstart", dragStart);
     card.addEventListener("dragend", dragEnd);
   }
 
   for (let empty of empties) {
-      const dragOver = function (evt) {
-          event.preventDefault();
-          console.log('over')
-      }
-      const dragEnter = function (evt) {
-          event.preventDefault();
-          this.classList.add('hovered')
-          console.log('enter');
-      }
-      const dragLeave = function () {
-        this.classList.remove('hovered')
-        console.log('leave');
-      }
-      const dragDrop = function () {
-        this.append(transfer)
-        console.log('drop');
-      }
-      empty.addEventListener("dragover", dragOver)
-      empty.addEventListener("dragenter", dragEnter)
-      empty.addEventListener("dragleave", dragLeave)
-      empty.addEventListener("drop", dragDrop)
+    const dragOver = function (evt) {
+      event.preventDefault();
+      console.log("over");
+    };
+    const dragEnter = function (evt) {
+      event.preventDefault();
+      this.classList.add("hovered");
+      console.log("enter");
+    };
+    const dragLeave = function () {
+      this.classList.remove("hovered");
+      console.log("leave");
+    };
+    const dragDrop = function () {
+      this.append(transfer);
+      console.log("drop");
+    };
+    empty.addEventListener("dragover", dragOver);
+    empty.addEventListener("dragenter", dragEnter);
+    empty.addEventListener("dragleave", dragLeave);
+    empty.addEventListener("drop", dragDrop);
   }
-  
 };
 
+export default dragAndDrop;
 
 
-dragAndDrop();
--- public/js/random-cards.js
+++ public/js/random-cards.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import Card from "./Card.js";
+import dragAndDrop from "./drag'n'drop.js";
 
 //получаем карты из json
 async function getCard() {
@@ -13,21 +14,34 @@ async function getCard() {
 const $firstPlayerDeck = document.querySelector(".first-player-deck");
 const $secondPlayerDeck = document.querySelector(".second-player-deck");
 
+//получаем рандомное число
 
 getCard().then((data) => {
-  for (let item of data) {
-    let card = new Card(
-      item.id,
-      item.title,
-      item.portrait,
-      item.price,
-      item.health,
-      item.attack
-    );
-    let $newCard = document.createElement('div')
-    $newCard.classList.add('card')
-    $newCard.setAttribute('draggable','true')
-    $newCard.innerHTML=`
+  //перемешать массив
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+  //функция добавления карт в колоду
+  function addCardsToDeck(deck) {
+    shuffleArray(data);
+    for (let item of data) {
+      let card = new Card(
+        item.id,
+        item.title,
+        item.portrait,
+        item.price,
+        item.health,
+        item.attack
+      );
+
+      let $newCard = document.createElement("div");
+      $newCard.classList.add("card");
+      $newCard.setAttribute("draggable", "true");
+      $newCard.setAttribute("id", item.id);
+      $newCard.innerHTML = `
     <img
     class="card-portrait"
     src="${card.portrait}"
@@ -37,9 +51,15 @@ getCard().then((data) => {
   <div class="cost">${card.price}</div>
   <div class="defence">${card.health}</div>
   <div class="attack">${card.attack}</div>
-  <div class="card-title">${card.title}</div>`
-  $firstPlayerDeck.appendChild($newCard);
+  <div class="card-title">${card.title}</div>`;
+
+      deck.appendChild($newCard);
+    }
   }
-  
-  
+  addCardsToDeck($firstPlayerDeck)
+  addCardsToDeck($secondPlayerDeck)
+
+  dragAndDrop();
+
+
 });

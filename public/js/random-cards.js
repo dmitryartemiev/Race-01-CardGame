45db68fd1f6b7ef03bb69365c64f3b06ea246d39
dmitryartemiev <dima77261@gmail.com>
2021-02-17 16:56:17 +0200

    game in progress

--- public/js/random-cards.js
+++ public/js/random-cards.js
@@ -1,8 +1,7 @@
 "use strict";
 
-import Card from './Card.js';
+import Card from "./Card.js";
 
-console.log(Card);
 //получаем карты из json
 async function getCard() {
   const response = await fetch("./js/cards.json");
@@ -10,13 +9,37 @@ async function getCard() {
   return [...data];
 }
 
-
 //Цикл добавления карты в колоду
-const firstPlayerDeck = document.querySelector(".first-player-deck");
-const secondPlayerDeck = document.querySelector(".second-player-deck");
+const $firstPlayerDeck = document.querySelector(".first-player-deck");
+const $secondPlayerDeck = document.querySelector(".second-player-deck");
+
 
 getCard().then((data) => {
   for (let item of data) {
-    console.log(item);
+    let card = new Card(
+      item.id,
+      item.title,
+      item.portrait,
+      item.price,
+      item.health,
+      item.attack
+    );
+    let $newCard = document.createElement('div')
+    $newCard.classList.add('card')
+    $newCard.setAttribute('draggable','true')
+    $newCard.innerHTML=`
+    <img
+    class="card-portrait"
+    src="${card.portrait}"
+    alt=""
+    draggable="false"
+  />
+  <div class="cost">${card.price}</div>
+  <div class="defence">${card.health}</div>
+  <div class="attack">${card.attack}</div>
+  <div class="card-title">${card.title}</div>`
+  $firstPlayerDeck.appendChild($newCard);
   }
+  
+  
 });
